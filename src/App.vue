d648fc74b73f5f5bddeae84b7c65e78f8e13fcc5
xJustFox <dodydaversa@gmail.com>
2024-03-01 15:02:41 +0100

    [Upgrade] Completed MILESTONE 4

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,33 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import {store} from './store.js';
+import axios from 'axios';
 
 export default {
+  data() {
+        return {
+            store,
+            projects: [],
+        }
+    },
   components: {
     AppHeader,
     AppMain,
     AppFooter
-  }
-}
+  },
+  created(){
+    this.getPorjects();
+  },
+  methods: {
+    getPorjects(){
+      axios.get(this.store.apiUrl).then((response) =>{
+        this.projects = response.data
+        console.log(this.projects);
+      })
+    }
+  },
+};
 </script>
 
 <template>
